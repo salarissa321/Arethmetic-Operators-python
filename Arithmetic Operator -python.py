c46e6b744e67f928e8df7ccd02f67e71c65419fb
salarissa321 <salarissa321@gmail.com>
2023-04-17 10:13:57 +0200

    add Arithmetic-python-Operators

---
+++ Arithmetic Operator -python.py
@@ -0,0 +1,62 @@
+
+
+#---------
+#--Arithmetic Operators----
+#-------------------
+# [+] Addition
+# [-] Subtraction
+# [*] Multiplication
+# [/] Division
+# [%] Modulus
+# [**] Exponent
+# [//] Floor Divion
+#---------------------
+
+
+# Addition
+
+print(1+5) #6
+print(-50 + 70) # 20
+print(1+7.98) #8.98
+print(1.2 + 1.2) # 2.4
+
+
+# Subtraction
+
+print(60-20) # 40
+print(-30 - 20) # -50
+print(-30- -10) # -20
+print(6.77 - 5.40) # 1.36999999999992
+
+
+# Multiplication
+
+print(5+100) # 105
+print(5+10*100) # 1005
+print((5+10) * 100) # 1500
+
+# Division
+
+print(100/20) # 5.0
+print(int(100/20)) # 5
+
+# Modulus
+
+print(8%2) # 0
+print(9%2) # 1
+print(20%4) # 0
+print(22%5) # 2
+
+# Exponent
+
+print(2 ** 5) # 32 (2*2*2*2*2) = 32
+print(5 ** 4) # 625 (5*5*5*5) = 625
+
+
+# Floor Division
+
+print(100 // 20) # 5
+print(110 // 20) # 5
+print(120 // 20) # 6
+print(140 // 20) # 7
+
